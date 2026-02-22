e9925648f66b362209d031f814240c4bf94d950c
Oscar <o9730654@gmail.com>
2026-02-21 19:55:41 -0700

    Agregando ruta de verificacion para Loader.io

--- Flaskk.py
+++ Flaskk.py
@@ -157,7 +157,10 @@ def actualizar_estado_pedido(current_user, pedido_id):
     finally:
         cur.close()
         conn.close()
-
+        
+@app.route('/loaderio-02da15920fabcf6b26e0709c27fafdd9.txt')
+def verify_loader_io():
+    return "loaderio-02da15920fabcf6b26e0709c27fafdd9"
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5001))
     app.run(host='0.0.0.0', port=port, debug=True)
